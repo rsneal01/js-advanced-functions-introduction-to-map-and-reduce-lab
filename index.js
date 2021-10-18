2ee23e595da40d371ce9e115f5563cb485f14117
rsneal01 <rsneal01@gmail.com>
2021-10-18 09:06:43 -0700

    commit tree dd8e17d21a6a24a31e8cbbbab2ac12291a89c8d3

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+const mapToNegativize = function(sourceArray){
+    return sourceArray.map(number => number * (-1))
+}
+const mapToNoChange = function(sourceArray){
+    return sourceArray.map(number => number)
+}
+const mapToDouble = function(sourceArray) {
+    return sourceArray.map(number => number * 2)
+}
+const mapToSquare = function(sourceArray) {
+    return sourceArray.map(number => number * number)
+}
+
+const reduceToTotal = function(sourceArray, startingPoint = 0) {
+    // return sourceArray.reduce(function(accumulator, element){return element + accumulator})
+    let total = startingPoint
+    for (const number of sourceArray) {
+        total += number;
+      };
+    
+      return total;
+}
+
+const reduceToAllTrue = function(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++) {
+        // if (sourceArray[i]){
+        //     return true;
+        // }
+        if (!sourceArray[i]) return false
+        
+    }
+
+    return true
+}
+
+const reduceToAnyTrue = function(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) return true
+    }
+    return false
+}
+ 
